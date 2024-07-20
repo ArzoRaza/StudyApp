dd6f82cdaabec7f1dc4329487704f9f8b27326ca
ArzoRaza <ccoding76@gmail.com>
2024-07-20 21:53:51 +0530

    Initializing some updates

--- screens/onboarding/onboarding.screen.tsx
+++ screens/onboarding/onboarding.screen.tsx
@@ -1,22 +1,28 @@
-import {  Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { useFonts, Raleway_700Bold } from "@expo-google-fonts/raleway";
 import { Nunito_400Regular, Nunito_700Bold } from "@expo-google-fonts/nunito";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function OnBoardingScreen() {
-    let [fontsloaded, fontError] = useFonts({
-        Raleway_700Bold,
-        Nunito_400Regular,
-        Nunito_700Bold,
-    });
+  let [fontsloaded, fontError] = useFonts({
+    Raleway_700Bold,
+    Nunito_400Regular,
+    Nunito_700Bold,
+  });
 
-    if(!fontsloaded && !fontError){
-        return null;
-    }
+  if (!fontsloaded && !fontError) {
+    return null;
+  }
+  
   return (
-    <linearGradient colors={["#E5ECF9", "#F^F&F9"]}>
+    <LinearGradient
+      colors={["#E5ECF9", "#F^F&F9"]}
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <View style={}>
+        
+      </View>
 
-    </linearGradient>
+    </LinearGradient>
   );
 }
-
